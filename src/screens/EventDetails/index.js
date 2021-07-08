4cda7702025947574631b5273f3edb53789e1b8c
JosephGabriel <josegabriel.jgs18@gmail.com>
2021-07-08 14:26:31 -0300

    Tela de detalhes finalizada

--- src/screens/EventDetails/index.js
+++ src/screens/EventDetails/index.js
@@ -1,34 +1,57 @@
-import React from 'react'
-import { View, Text, Image } from 'react-native'
-import { Subheading, Headline,Chip, Button,Paragraph } from 'react-native-paper'
+import React from "react";
+import { View, Image, ScrollView } from "react-native";
+import {
+  Subheading,
+  Headline,
+  Chip,
+  Button,
+  Paragraph,
+} from "react-native-paper";
 
-const EventDetails = ({item}) => {
+import Container from "../../components/Container";
+import EventCardCarrousel from "../../components/EventCardCarrousel";
 
+const EventDetails = ({ item }) => {
   // const {title, description,price} = item
 
   return (
-    <View style={{flex:1}}>
-      <Image source={{ uri:"https://wallpapercave.com/wp/wp2737727.jpg"}} resizeMode="cover" style={{width:'100%',height:250}} />
-      <View style={{padding:15}}>
-        <View style={{flexDirection:"row", justifyContent:"space-between", marginBottom:15}}>
-        <Headline style={{fontWeight:'600'}}>Artes & Matemática</Headline>
-        <Subheading style={{fontWeight:'bold', color:'#6200EE'}}>R$150,00</Subheading>
+    <ScrollView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <Image
+        source={{ uri: "https://wallpapercave.com/wp/wp2737727.jpg" }}
+        resizeMode="cover"
+        style={{ width: "100%", height: 250 }}
+      />
+      <View style={{ padding: 15 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 15,
+          }}
+        >
+          <Headline style={{ fontWeight: "600" }}>Artes & Matemática</Headline>
+          <Subheading style={{ fontWeight: "bold", color: "#6200EE" }}>
+            R$150,00
+          </Subheading>
         </View>
-        <View style={{flexDirection:"row", justifyContent:"space-between"}}>
-        <View style={{flexDirection:"row"}}>
-        <Chip icon="check">Online</Chip>
-        <Chip icon="check">Example Chip</Chip>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <View style={{ flexDirection: "row" }}>
+            <Chip style={{ marginRight: 10 }}>Online</Chip>
+            <Chip>Example Chip</Chip>
+          </View>
+          <Button mode="contained">Comprar</Button>
         </View>
-        <Button mode="contained" >Comprar</Button>
-        </View>
-        <Paragraph style={{marginVertical:20}}>
-          Finely crafted graphite wooden pencil. Premium quality
-          lead guarantees smoothness at all times. Ideal for artists and designers. 
-          HB hardness only.
+        <Paragraph style={{ marginVertical: 20 }}>
+          Finely crafted graphite wooden pencil. Premium quality lead guarantees
+          smoothness at all times. Ideal for artists and designers. HB hardness
+          only.
         </Paragraph>
       </View>
-    </View>
-  )
-}
+      <Container padding={20} background="#eee">
+        <EventCardCarrousel isTitled />
+      </Container>
+    </ScrollView>
+  );
+};
 
-export default EventDetails
+export default EventDetails;
